b84d3237b931be25a145f9cea6ad54b31836693e
Wzyan <3101885298@qq.com>
2022-06-21 09:57:55 +0800

    feat: vscode-weiyi-tools | 实现live-server错误提示

--- commandIns/liveServer.js
+++ commandIns/liveServer.js
@@ -1,10 +1,19 @@
 let name = "liveServer";
 const { exec } = require("node:child_process");
+const vscodeApi = require("../utils/vscode-api");
 const shell = require("shelljs");
 module.exports = {
   name,
   implementation: function (url) {
     shell.cd(url.path);
-    exec(`live-server`, function (error, stdout, stderr) {});
+    try {
+      exec(`live-server`, function (error, stdout, stderr) {
+        if (error) {
+          vscodeApi
+            .$toast()
+            .err("依赖缺失，请执行 npm i live-server -g 进行安装");
+        }
+      });
+    } catch (error) {}
   },
 };
