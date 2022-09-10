c20deeeaf4337d02fc39ce79e5b3622b929b3e39
Wzyan <3101885298@qq.com>
2022-09-10 22:52:10 +0800

    feat: insertLog支持shell

--- commandIns/insertLog.js
+++ commandIns/insertLog.js
@@ -4,15 +4,11 @@ let vscodeApi = new VscodeApi(name);
 module.exports = {
   name,
   implementation: function () {
-    let fileType = 'js' // 文件类型
-    if (vscodeApi.currentDocumentFileType === 'sh') {
-
-    }
     // 获取当前选中的内容
     let text = vscodeApi.selectText;
     function handleText(text) {
       let handleText = ''
-      switch (fileType) {
+      switch (vscodeApi.currentDocumentFileType) {
         case 'js':
           let consoleKeyword = "log";
           if (text.indexOf("err") !== -1) {
@@ -25,7 +21,7 @@ module.exports = {
           break;
 
         case 'sh':
-          handleText = `echo $${text}`
+          handleText = `echo $${text}\n`
           break;
         default:
           break;
@@ -34,10 +30,9 @@ module.exports = {
     }
     // 在这里拼写console语句
     const logToInsert = handleText(text);
+    console.log('handleText: ', handleText);
     // 执行插行方法
-    text
-      ? vscodeApi.insertTextToNextLine(logToInsert)
-      : vscodeApi.insertTextToNextLine("console.log();");
+    vscodeApi.insertTextToNextLine(logToInsert)
     vscodeApi.emit();
   },
 };
--- sh/publish.sh
+++ sh/publish.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/bash
+# 解决相对路径问题 执行脚本前先进入脚本所在的目录
+cd $(dirname $0)
+source $HOME/.nvm/nvm.sh
 nvm use v14
+echo $(node -v)
 vsce publish patch
+# 执行完成后切换会上一次目录 相当于 `cd OLDPWD`
+cd -
