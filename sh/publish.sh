f69abd7eafec6dcee85c034614a3fcec5a6d52c2
Wzyan <3101885298@qq.com>
2022-09-18 21:43:45 +0800

    发布脚本如果没有输入commit信息就中止发布

--- sh/publish.sh
+++ sh/publish.sh
@@ -1,19 +1,20 @@
 #!/bin/zsh
 node -v
 echo "$1"
-
-# if [  ]; then
-#     # if body
-# fi
-# if [[ "${1}" == "${}" ]]; then
-#     echo "The two strings are the same"
-# fi
+# 如果没有输入commit信息就中止发布
+if [[ -z "${1}" ]]; then
+    echo "请输入本次修改commit信息"
+    exit 1
+fi
 # # 更新下在线文档信息
 jsdoc utils/node-api.js
 jsdoc utils/vscode-api.js
 # # 发布最新的文档信息 访问http://vscode-extension-api.surge.sh/global.html#fileIsExist即可查看
 surge teardown vscode-extension-api.surge.sh
 surge utils/out vscode-extension-api.surge.sh
+# 进行git仓库的上传
 git add .
 git commit -m "$1"
+git push
+# 更新vscode插件
 vsce publish patch
