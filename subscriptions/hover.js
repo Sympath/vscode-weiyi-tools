75d68ce92bbc852b6092ce3c78bad733902c836b
Wzyan <3101885298@qq.com>
2022-09-28 11:22:44 +0800

    feat: 实现切换目录兼容win 替换为_fsPath 1

--- subscriptions/hover.js
+++ subscriptions/hover.js
@@ -5,6 +5,7 @@ let vscodeApi = new VscodeApi(name);
 const vscode = vscodeApi.vscode
 module.exports = vscode.languages.registerHoverProvider("javascript", {
     provideHover: (document, position) => {
+        let url = '111'
         return new vscode.Hover(new vscode.MarkdownString(`![](${url})`));
     }
 })
