2485a28601f648dee54eb836c0bbaa3acab891ce
Wzyan <3101885298@qq.com>
2022-09-30 17:02:14 +0800

    feat: 支持面试目录打开

--- commandIns/oftenUse.js
+++ commandIns/oftenUse.js
@@ -11,10 +11,26 @@ module.exports = {
                 quickItem: {
                     description: '书/电影/音乐'
                 }// 选项详细信息 label: string;description ?: string;detail ?: string;picked ?: boolean;alwaysShow ?: boolean;
+            },
+            "interview": {
+                quickItem: {
+                    description: '面试'
+                },// 选项详细信息 label: string;description ?: string;detail ?: string;picked ?: boolean;alwaysShow ?: boolean;
+                path: '/Users/wzyan/Documents/selfspace/note-all/未雨绸缪'
             }
         }
         let quickPickOptions = Object.keys(optionMap).map(key => ({ label: key, ...(optionMap[key].quickItem) }))
-        let { label: choose } = await vscodeApi.$quickPick(quickPickOptions)
-        vscodeApi.runGlobalCommand('code', [`${root}/${choose}.md`])
+        let chooseItem = await vscodeApi.$quickPick(quickPickOptions)
+        if (!chooseItem) {
+            return
+        }
+        chooseItem = optionMap[chooseItem.label]
+        let { path } = chooseItem
+        // 如果有自定义路径，就打开对应路径即可
+        if (path) {
+            vscodeApi.runGlobalCommand('code', [path])
+        } else {
+            vscodeApi.runGlobalCommand('code', [`${root}/${choose}.md`])
+        }
     },
 };
